3ae5137eee74e4663cebd4b4f488a068c6de5b95
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2017-12-16 12:38:32 -0600

    Implement Reservation form

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public submitted: boolean;
+  roomSearch: FormGroup;
+
+  ngOnInit(): void {
+    this.roomSearch = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl('')
+    });
+  }
+
+  onSubmit({value, valid}: { value: Roomsearch, valid: boolean }) {
+    console.log(value);
+    console.log(valid);
+  }
+}
+
+export interface Roomsearch {
+  checkin: string;
+  checkout: string;
 }
